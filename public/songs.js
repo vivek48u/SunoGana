9f2fcb99bef5e324b925af9179ae7acd628b3058
Vivek Tulshiram Patil <patilvivek2210@gmail.com>
2026-01-11 21:24:47 +0530

    Update songs.js

--- public/songs.js
+++ public/songs.js
@@ -1,19 +1,32 @@
-const songs = [
-  {
-    name: "Night Drive",
-    artist: "NCS",
-    url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144515/Raanjhanaa_From_Raanjhanaa-128kbps_fas9wb.mp3"
+const playlists = {
+  ncs: {
+    title: "NCS Hits",
+    description: "No Copyright Songs",
+    cover: "img/cover.jpg",
+    songs: [
+      {
+        name: "Sky High",
+        artist: "NCS",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144515/Raanjhanaa_From_Raanjhanaa-128kbps_fas9wb.mp3"
+      },
+      {
+        name: "Energy",
+        artist: "NCS",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144514/Dhurandhar_Title_Track-64kbps_l1d6xf.mp3"
+      }
+    ]
   },
-  {
-    name: "Sky High",
-    artist: "NCS",
-    url:" https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144514/Dhurandhar_Title_Track-64kbps_l1d6xf.mp3"
-  },
-  {
-    name: "Dreamer",
-    artist: "NCS",
-    url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144513/Chaand_Taare-64kbps_gm0xdd.mp3"
-  }
-];
 
-console.log("Songs loaded:", songs);
+  lofi: {
+    title: "Lofi Beats",
+    description: "Chill & Relax",
+    cover: "img/cover.jpg",
+    songs: [
+      {
+        name: "Late Night",
+        artist: "Lofi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144513/Chaand_Taare-64kbps_gm0xdd.mp3"
+      }
+    ]
+  }
+};
