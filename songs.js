f44075970fda99dba33fe0412fcd1d60f529b5d1
Vivek Tulshiram Patil <patilvivek2210@gmail.com>
2026-01-11 20:55:50 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -2,16 +2,16 @@ const songs = [
   {
     name: "Night Drive",
     artist: "NCS",
-    url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/song1.mp3"
+    url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144515/Raanjhanaa_From_Raanjhanaa-128kbps_fas9wb.mp3"
   },
   {
     name: "Sky High",
     artist: "NCS",
-    url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/song2.mp3"
+    url:" https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144514/Dhurandhar_Title_Track-64kbps_l1d6xf.mp3"
   },
   {
     name: "Dreamer",
     artist: "NCS",
-    url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/song3.mp3"
+    url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144513/Chaand_Taare-64kbps_gm0xdd.mp3"
   }
 ];
