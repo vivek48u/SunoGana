5530f40fe643d0e13f585f517163da2c9db0ed6c
vivek48u <patilvivek2210@gmail.com>
2026-01-11 22:15:15 +0530

    updated songs folder system with cloudinary

--- public/script.js
+++ public/script.js
@@ -1,217 +1,151 @@
-console.log('Lets write JavaScript');
-let currentSong = new Audio();
-let songs;
-let currFolder;
+console.log("Spotify Clone - Playlist Version");
 
+let currentSong = new Audio();
+let songs = [];
+let currentIndex = 0;
+
+/* ---------------- TIME FORMAT ---------------- */
 function secondsToMinutesSeconds(seconds) {
-    if (isNaN(seconds) || seconds < 0) {
-        return "00:00";
-    }
+    if (isNaN(seconds) || seconds < 0) return "00:00";
 
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
 
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-
-    return `${formattedMinutes}:${formattedSeconds}`;
+    return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
 }
 
-async function getSongs(folder) {
-    currFolder = folder;
-    let a = await fetch(`/${folder}/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
-    }
+/* ---------------- LOAD PLAYLIST (FOLDER STYLE) ---------------- */
+function loadPlaylist(key) {
+    const playlist = playlists[key];
+    if (!playlist) return;
 
+    songs = playlist.songs;
+    currentIndex = 0;
 
+    const songUL = document.querySelector(".songlist ul");
+    songUL.innerHTML = "";
 
-    // Show all the songs in the playlist
-    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
-    songUL.innerHTML = ""
-    for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
-                            <div class="info">
-                                <div> ${song.replaceAll("%20", " ")}</div>
-                                <div>Harry</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img class="invert" src="img/play.svg" alt="">
-                            </div> </li>`;
-    }
+    songs.forEach((song, index) => {
+        songUL.innerHTML += `
+        <li data-index="${index}">
+            <img class="invert" width="34" src="img/music.svg">
+            <div class="info">
+                <div>${song.name}</div>
+                <div>${song.artist}</div>
+            </div>
+            <div class="playnow">
+                <span>Play Now</span>
+                <img class="invert" src="img/play.svg">
+            </div>
+        </li>`;
+    });
 
-    // Attach an event listener to each song
-    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    Array.from(songUL.children).forEach(li => {
+        li.addEventListener("click", () => {
+            const index = parseInt(li.dataset.index);
+            playMusic(index);
+        });
+    });
 
-        })
-    })
-
-    return songs
+    playMusic(0, true);
 }
 
-const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/` + track
+/* ---------------- PLAY MUSIC ---------------- */
+function playMusic(index, pause = false) {
+    currentIndex = index;
+    currentSong.src = songs[index].url;
+
     if (!pause) {
-        currentSong.play()
-        play.src = "img/pause.svg"
-    }
-    document.querySelector(".songinfo").innerHTML = decodeURI(track)
-    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
-
-
-}
-
-async function displayAlbums() {
-    console.log("displaying albums")
-    let a = await fetch(`/songs/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a")
-    let cardContainer = document.querySelector(".cardcontainer")
-    let array = Array.from(anchors)
-    for (let index = 0; index < array.length; index++) {
-        const e = array[index];
-        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
-            let folder = new URL(e.href).pathname.split("/").filter(Boolean).pop()
-
-            // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
-            if (!a.ok) {
-                console.error("info.json missing for album:", folder);
-                continue;
-            }
-            let response = await a.json();
-
-            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
-            
-                 <div  class="play">
-                            <img src="img/play.svg" alt="">
-                </div>
-            
-
-            <img src="/songs/${folder}/cover.jpg" alt="">
-            <h2>${response.title}</h2>
-            <p>${response.description}</p>
-        </div>`
-        }
+        currentSong.play();
+        play.src = "img/pause.svg";
     }
 
-    // Load the playlist whenever card is clicked
-    Array.from(document.getElementsByClassName("card")).forEach(e => {
-        e.addEventListener("click", async item => {
-            console.log("Fetching Songs")
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
-            playMusic(songs[0])
+    document.querySelector(".songinfo").innerText = songs[index].name;
+    document.querySelector(".songtime").innerText = "00:00 / 00:00";
 
-        })
-    })
+    document.querySelectorAll(".songlist li").forEach(li => li.classList.remove("active"));
+    document.querySelectorAll(".songlist li")[index].classList.add("active");
 }
 
-async function main() {
-    // Get the list of all the songs
-    await getSongs("songs/ncs")
-    playMusic(songs[0], true)
+/* ---------------- MAIN ---------------- */
+function main() {
 
-    // Display all the albums on the page
-    await displayAlbums()
+    // Load default playlist (change key if needed)
+    loadPlaylist("ncs");
 
-
-    // Attach an event listener to play, next and previous
+    /* Play / Pause */
     play.addEventListener("click", () => {
         if (currentSong.paused) {
-            currentSong.play()
-            play.src = "img/pause.svg"
+            currentSong.play();
+            play.src = "img/pause.svg";
+        } else {
+            currentSong.pause();
+            play.src = "img/play.svg";
         }
-        else {
-            currentSong.pause()
-            play.src = "img/play.svg"
-        }
-    })
+    });
 
-    // Listen for timeupdate event
+    /* Time Update */
     currentSong.addEventListener("timeupdate", () => {
-        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
-        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
-    })
+        document.querySelector(".songtime").innerText =
+            `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
 
-    // Add an event listener to seekbar
-    document.querySelector(".seekbar").addEventListener("click", e => {
-        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-        document.querySelector(".circle").style.left = percent + "%";
-        currentSong.currentTime = ((currentSong.duration) * percent) / 100
-    })
+        document.querySelector(".circle").style.left =
+            (currentSong.currentTime / currentSong.duration) * 100 + "%";
+    });
 
-    // Add an event listener for hamburger
-    document.querySelector(".hamburger").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "0"
-    })
+    /* Auto play next */
+    currentSong.addEventListener("ended", () => {
+        if (currentIndex < songs.length - 1) {
+            playMusic(currentIndex + 1);
+        }
+    });
 
-    // Add an event listener for close button
-    document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "-120%"
-    })
+    /* Seekbar */
+    document.querySelector(".seekbar").addEventListener("click", (e) => {
+        const percent = e.offsetX / e.target.getBoundingClientRect().width;
+        currentSong.currentTime = percent * currentSong.duration;
+    });
 
-    // Add an event listener to previous
+    /* Previous */
     previous.addEventListener("click", () => {
-        currentSong.pause()
-        console.log("Previous clicked")
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-        if ((index - 1) >= 0) {
-            playMusic(songs[index - 1])
+        if (currentIndex > 0) {
+            playMusic(currentIndex - 1);
         }
-    })
+    });
 
-    // Add an event listener to next
+    /* Next */
     next.addEventListener("click", () => {
-        currentSong.pause()
-        console.log("Next clicked")
-
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-        if ((index + 1) < songs.length) {
-            playMusic(songs[index + 1])
+        if (currentIndex < songs.length - 1) {
+            playMusic(currentIndex + 1);
         }
-    })
+    });
 
-    // Add an event to volume
-    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
-        console.log("Setting volume to", e.target.value, "/ 100")
-        currentSong.volume = parseInt(e.target.value) / 100
+    /* Volume */
+    document.querySelector(".range input").addEventListener("change", (e) => {
+        currentSong.volume = e.target.value / 100;
+    });
+
+    /* Mute */
+    document.querySelector(".volume img").addEventListener("click", (e) => {
         if (currentSong.volume > 0) {
-            document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
-        }
-    })
-
-    // Add event listener to mute the track
-    document.querySelector(".volume>img").addEventListener("click", e => {
-        if (e.target.src.includes("volume.svg")) {
-            e.target.src = e.target.src.replace("volume.svg", "mute.svg")
             currentSong.volume = 0;
-            document.querySelector(".range").getElementsByTagName("input")[0].value = 0;
+            e.target.src = "img/mute.svg";
+            document.querySelector(".range input").value = 0;
+        } else {
+            currentSong.volume = 0.1;
+            e.target.src = "img/volume.svg";
+            document.querySelector(".range input").value = 10;
         }
-        else {
-            e.target.src = e.target.src.replace("mute.svg", "volume.svg")
-            currentSong.volume = .10;
-            document.querySelector(".range").getElementsByTagName("input")[0].value = 10;
-        }
+    });
 
-    })
+    /* Mobile Menu */
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "0";
+    });
 
-
-
-
-
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-120%";
+    });
 }
 
-main() 
+main();
---
+++ public/songs.js
@@ -0,0 +1,109 @@
+const playlists = {
+
+  ahirani: {
+    title: "Ahirani Songs",
+    description: "Ahirani Collection",
+    cover: "img/ahirani.jpg",
+    songs: [
+      {
+        name: "Ahirani Song 1",
+        artist: "Ahirani",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/ahirani1.mp3"
+      }
+    ]
+  },
+
+  bollywood: {
+    title: "Bollywood Songs",
+    description: "Hindi Bollywood Hits",
+    cover: "img/bollywood.jpg",
+    songs: [
+      {
+        name: "Bollywood Song 1",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144514/Dhurandhar_Title_Track-64kbps_l1d6xf.mp3"
+      }
+    ]
+  },
+
+  marathi: {
+    title: "Marathi Songs",
+    description: "Marathi Collection",
+    cover: "img/marathi.jpg",
+    songs: [
+      {
+        name: "Marathi Song 1",
+        artist: "Marathi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768147636/Kaali_Bindi_gppblz.mp3"
+      }
+    ]
+  },
+
+  motivational: {
+    title: "Motivational Songs",
+    description: "Energy & Motivation",
+    cover: "img/motivation.jpg",
+    songs: [
+      {
+        name: "Motivation Song 1",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149355/Jeetenge-128kbps_begtjh.mp3"
+      }
+    ]
+  },
+
+  ncs: {
+    title: "NCS",
+    description: "No Copyright Songs",
+    cover: "img/ncs.jpg",
+    songs: [
+      {
+        name: "NCS Song 1",
+        artist: "NCS",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149397/Ram_Siya_Ram_From_Adipurush_dxu5il.mp3"
+      }
+    ]
+  },
+
+  old: {
+    title: "Old Songs",
+    description: "Old is Gold",
+    cover: "img/old.jpg",
+    songs: [
+      {
+        name: "Old Song 1",
+        artist: "Old Hits",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149462/Ham_Tere_Pyar_Mein_PagaiWorld.com_gy3you.mp3"
+      }
+    ]
+  },
+
+  punjabi: {
+    title: "Punjabi Songs",
+    description: "Punjabi Beats",
+    cover: "img/punjabi.jpg",
+    songs: [
+      {
+        name: "Punjabi Song 1",
+        artist: "Punjabi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149555/Haseen_PagaiWorld.com_jttxih.mp3"
+      }
+    ]
+  },
+
+  romantic: {
+    title: "Romantic Songs",
+    description: "Love & Romance",
+    cover: "img/romantic.jpg",
+    songs: [
+      {
+        name: "Romantic Song 1",
+        artist: "Romantic",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149544/Vaara_Re-128kbps_dmj1hd.mp3"
+      }
+    ]
+  }
+
+};
+
+console.log("Playlists loaded:", Object.keys(playlists));
