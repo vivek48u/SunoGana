71e3b4c32031f2d16077a5717d4cd951cba4462f
Vivek Tulshiram Patil <patilvivek2210@gmail.com>
2026-01-11 20:53:36 +0530

    Add songs.js with song list and URLs

---
+++ songs.js
@@ -0,0 +1,17 @@
+const songs = [
+  {
+    name: "Night Drive",
+    artist: "NCS",
+    url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/song1.mp3"
+  },
+  {
+    name: "Sky High",
+    artist: "NCS",
+    url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/song2.mp3"
+  },
+  {
+    name: "Dreamer",
+    artist: "NCS",
+    url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/song3.mp3"
+  }
+];
