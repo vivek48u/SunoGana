d5df4ad1e049d1dad75c637c33de3976bb219956
Vivek Tulshiram Patil <patilvivek2210@gmail.com>
2026-01-11 21:47:06 +0530

    Update songs.js

--- public/songs.js
+++ public/songs.js
@@ -1,32 +1,109 @@
 const playlists = {
-  ncs: {
-    title: "NCS Hits",
-    description: "No Copyright Songs",
-    cover: "img/cover.jpg",
+
+  ahirani: {
+    title: "Ahirani Songs",
+    description: "Ahirani Collection",
+    cover: "img/ahirani.jpg",
     songs: [
       {
-        name: "Sky High",
-        artist: "NCS",
-        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144515/Raanjhanaa_From_Raanjhanaa-128kbps_fas9wb.mp3"
-      },
-      {
-        name: "Energy",
-        artist: "NCS",
-        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144514/Dhurandhar_Title_Track-64kbps_l1d6xf.mp3"
+        name: "Ahirani Song 1",
+        artist: "Ahirani",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/ahirani1.mp3"
       }
     ]
   },
 
-  lofi: {
-    title: "Lofi Beats",
-    description: "Chill & Relax",
-    cover: "img/cover.jpg",
+  bollywood: {
+    title: "Bollywood Songs",
+    description: "Hindi Bollywood Hits",
+    cover: "img/bollywood.jpg",
     songs: [
       {
-        name: "Late Night",
-        artist: "Lofi",
-        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144513/Chaand_Taare-64kbps_gm0xdd.mp3"
+        name: "Bollywood Song 1",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/bollywood1.mp3"
+      }
+    ]
+  },
+
+  marathi: {
+    title: "Marathi Songs",
+    description: "Marathi Collection",
+    cover: "img/marathi.jpg",
+    songs: [
+      {
+        name: "Marathi Song 1",
+        artist: "Marathi",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/marathi1.mp3"
+      }
+    ]
+  },
+
+  motivational: {
+    title: "Motivational Songs",
+    description: "Energy & Motivation",
+    cover: "img/motivation.jpg",
+    songs: [
+      {
+        name: "Motivation Song 1",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/motivation1.mp3"
+      }
+    ]
+  },
+
+  ncs: {
+    title: "NCS",
+    description: "No Copyright Songs",
+    cover: "img/ncs.jpg",
+    songs: [
+      {
+        name: "NCS Song 1",
+        artist: "NCS",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/ncs1.mp3"
+      }
+    ]
+  },
+
+  old: {
+    title: "Old Songs",
+    description: "Old is Gold",
+    cover: "img/old.jpg",
+    songs: [
+      {
+        name: "Old Song 1",
+        artist: "Old Hits",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/old1.mp3"
+      }
+    ]
+  },
+
+  punjabi: {
+    title: "Punjabi Songs",
+    description: "Punjabi Beats",
+    cover: "img/punjabi.jpg",
+    songs: [
+      {
+        name: "Punjabi Song 1",
+        artist: "Punjabi",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/punjabi1.mp3"
+      }
+    ]
+  },
+
+  romantic: {
+    title: "Romantic Songs",
+    description: "Love & Romance",
+    cover: "img/romantic.jpg",
+    songs: [
+      {
+        name: "Romantic Song 1",
+        artist: "Romantic",
+        url: "https://res.cloudinary.com/YOUR_CLOUD_NAME/video/upload/romantic1.mp3"
       }
     ]
   }
+
 };
+
+console.log("Playlists loaded:", Object.keys(playlists));
