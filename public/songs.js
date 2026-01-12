c0d7e907d6f3f7122bf0b3232ce127691b6a92c2
Vivek Tulshiram Patil <patilvivek2210@gmail.com>
2026-01-12 15:20:42 +0530

    Update song names and URLs in playlists

--- public/songs.js
+++ public/songs.js
@@ -19,9 +19,44 @@ const playlists = {
     cover: "img/bollywood.jpg",
     songs: [
       {
-        name: "Bollywood Song 1",
+        name: "Dhurandhar_Title_Track-64kbps_l1d6xf.mp3",
         artist: "Bollywood",
         url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144514/Dhurandhar_Title_Track-64kbps_l1d6xf.mp3"
+      },
+      {
+        name: "Raanjhanaa_From_Raanjhanaa-128kbps_fas9wb.mp3",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144515/Raanjhanaa_From_Raanjhanaa-128kbps_fas9wb.mp3"
+      },
+      {
+        name: "Aawaara_Angaara-64kbps_qjik06.mp3",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144513/Aawaara_Angaara-64kbps_qjik06.mp3"
+      },
+      {
+        name: "Run_Down_The_City_Monica-64kbps_wpxo8e.mp3",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768144513/Run_Down_The_City_Monica-64kbps_wpxo8e.mp3"
+      },
+      {
+        name: "Tu-Hain-Toh-Main-Hoon",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768206490/Tu-Hain-Toh-Main-Hoon_pagalworld.group_zkd4os.mp3"
+      },
+      {
+        name: "Khairiyat-Pucho",
+        artist: "Bollywood",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768206495/Khairiyat-Pucho_pagalworld.group_jg1zjo.mp3"
+      },
+      {
+        name: "Zamaana-Lage",
+        artist: "Bollywood",
+        url:"https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768206495/Zamaana-Lage_pagalworld.group_w6sg7r.mp3"
+      },
+      {
+        name: "Channa-Mereya",
+        artist: "Bollywood",
+        url:"https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768206497/Channa-Mereya_pagalworld.group_zutwjt.mp3"
       }
     ]
   },
@@ -32,10 +67,30 @@ const playlists = {
     cover: "img/marathi.jpg",
     songs: [
       {
-        name: "Marathi Song 1",
+        name: "Kaali_Bindi_gppblz.mp3",
         artist: "Marathi",
         url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768147636/Kaali_Bindi_gppblz.mp3"
-      }
+      },
+      {
+        name: "Akashi_Chandra_Chandanya_donaj0.mp3",
+        artist: "Marathi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768147642/Akashi_Chandra_Chandanya_donaj0.mp3"
+      },
+      {
+        name: "Pakhara_Aazad_Kela_Tula_xqhcix.mp3",
+        artist: "Marathi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768147638/Pakhara_Aazad_Kela_Tula_xqhcix.mp3"
+      },
+      {
+        name: "Tambdi_Chamdi_-_PagalHits_bumuzo.mp3",
+        artist: "Marathi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768147592/Tambdi_Chamdi_-_PagalHits_bumuzo.mp3"
+      },
+      {
+        name: "Shaky_jqbz1x.mp3",
+        artist: "Marathi",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768207101/Shaky_jqbz1x.mp3"
+      },
     ]
   },
 
@@ -45,23 +100,98 @@ const playlists = {
     cover: "img/motivation.jpg",
     songs: [
       {
-        name: "Motivation Song 1",
+        name: "Jeetenge-128kbps",
         artist: "Motivational",
         url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149355/Jeetenge-128kbps_begtjh.mp3"
-      }
+      },
+      {
+        name: "Zinda_From_Bhaag_Milkha",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768207794/Zinda_From_Bhaag_Milkha_Bhaag-64kbps_cvfdoi.mp3"
+      },
+      {
+        name: "Brothers_Anthem",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768207973/Brothers_Anthem-320kbps_p1qyow.mp33"
+      },
+       {
+        name: "Mann_Mein_Shiva",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768208403/Mann_Mein_Shiva-320kbps_klijbo.mp3"
+      },
+       {
+        name: "Toofan-128kbps",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768208733/Toofan-128kbps_djxeon.mp3"
+      },
+       {
+        name: "Mard_Maratha -Panipat",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768208744/Mard_Maratha_-_Panipat_128_Kbps_y7v7wz.mp3"
+      },
+       {
+        name: "Chak_De_India-320kbps",
+        artist: "Motivational",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768208747/Chak_De_India-320kbps_alhz3a.mp3"
+      },
+
     ]
   },
 
   ncs: {
-    title: "NCS",
-    description: "No Copyright Songs",
+    title: "Devetional Songs",
+    description: "Bhakti geet/Bhajan",
     cover: "img/ncs.jpg",
     songs: [
       {
-        name: "NCS Song 1",
-        artist: "NCS",
-
+        name: "Ya-Re-Ya-Sare-Ya",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768207950/Ya-Re-Ya-Sare-Ya_gngidd.mp3"
+      },
+      {
+        name: "Ram_Siya_Ram_From_Adipurus",
+        artist: "Bhakti geet",
         url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149397/Ram_Siya_Ram_From_Adipurush_dxu5il.mp3"
+      },
+      {
+        name: "Hanuman_Chalisa",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209905/Hanuman_Chalisa-64kbps_dousqx.mp3"
+      },
+      {
+        name: "Aala_Re_Aala_Ganesha",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209902/Aala_Re_Aala_Ganesha-64kbps_apqav3.mp3"
+      },
+      {
+        name: "Hey_Dukh_Bhanjan_Maruti_Nandan",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209901/Hey_Dukh_Bhanjan_Maruti_Nandan-320kbps_gzpu9n.mp3"
+      },
+      {
+        name: "Shendur_Lal_Chadhayo-64kbps",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209899/Shendur_Lal_Chadhayo-64kbps_vms6c3.mp3"
+      },
+      {
+        name: "Mangal_Bhavan_Amangal_Hari",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209897/Mangal_Bhavan_Amangal_Hari_128-_PagalWorld.Org.Im_ik1r4k.mp3"
+      },
+      {
+        name: "Deva_Shree_Ganesha",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209893/Deva_Shree_Ganesha-320kbps_ockfv9.mp3"
+      },
+      {
+        name: "Bappa_Morya_Re-64kbps",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209875/Bappa_Morya_Re-64kbps_dipa6g.mp3"
+      },
+       {
+        name: "Gajanana",
+        artist: "Bhakti geet",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768209873/Gajanana-128kbps_iwlkvg.mp3"
       }
     ]
   },
@@ -72,9 +202,29 @@ const playlists = {
     cover: "img/old.jpg",
     songs: [
       {
-        name: "Old Song 1",
+        name: "Ham_Tere_Pyar_Mein",
         artist: "Old Hits",
         url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768149462/Ham_Tere_Pyar_Mein_PagaiWorld.com_gy3you.mp3"
+      },
+      {
+        name: "Jaadu_Teri_Nazar",
+        artist: "Old Hits",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768211163/Jaadu_Teri_Nazar-320kbps_da3a7a.mp3"
+      },
+      {
+        name: "Shayad_Meri_Shaadi",
+        artist: "Old Hits",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768211163/Shayad_Meri_Shaadi-320kbps_t3vpva.mp3"
+      },
+      {
+        name: "Kamariya_Lachke_Re",
+        artist: "Old Hits",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768211157/Kamariya_Lachke_Re-320kbps_ynr0m8.mp3"
+      },
+      {
+        name: "Dil_Ne_Yeh_Kaha_Hain_Dil_Se",
+        artist: "Old Hits",
+        url: "https://res.cloudinary.com/dnkyg3yy3/video/upload/v1768211154/Dil_Ne_Yeh_Kaha_Hain_Dil_Se-64kbps_h0kzb9.mp3"
       }
     ]
   },
@@ -106,7 +256,6 @@ const playlists = {
   }
 
 };
-
 
 console.log("Playlists loaded:", Object.keys(playlists));
 
