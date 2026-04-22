b8c142968e96b7f5c4998b2a533cd1f6dc9a47f8
Vivek Tulshiram Patil <patilvivek2210@gmail.com>
2026-04-22 06:04:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const serveIndex = require("serve-index")
 const app = express()
 const PORT = 3000
 
+app.get('/robots.txt', (req, res) => {
+  res.type('text/plain');
+  res.send("User-agent: *\nAllow: /");
+});
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use(
@@ -12,10 +16,6 @@ app.use(
     express.static(path.join(__dirname, "songs")),
     serveIndex(path.join(__dirname, "songs"), { icons: true })
 )
-app.get('/robots.txt', (req, res) => {
-  res.type('text/plain');
-  res.send("User-agent: *\nAllow: /");
-});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
